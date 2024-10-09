a98a4e1ed24a5553de4914aacb1805079aef0235
jhonatanpinheir <134715337+jhonatanpinheir@users.noreply.github.com>
2024-10-09 08:16:16 -0300

    aula rede social

---
+++ graficos/quantidadeUsuarios.js
@@ -0,0 +1,24 @@
+async function quantidadeUsuarios() {
+    const url = 'https://raw.githubusercontent.com/guilhermeomrails/api/main/numero-usuarios.json'
+    const res = await fetch(url)
+    const dados = await res.json()
+    const nomeDasRedes = Object.keys(dados)
+    const quantidadeUsuarios = Object.values(dados)
+  
+  }
+  
+  quantidadeUsuarios()
+  const data = [
+    {
+      x: 'nomeDasRedes',
+      y: quantidadeDeUsuarios,
+      type: 'bar',
+      marker: {
+        color: getComputedStyle(document.body).getPropertyValue('--primary-color')
+      }
+    }
+  ]
+  const grafico = document.createElement('div')
+grafico.className = 'grafico'
+document.getElementById('graficos-container').appendChild(grafico)
+Plotly.newPlot(grafico, data)
